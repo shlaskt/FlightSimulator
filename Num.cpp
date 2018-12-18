e7345fd8e8a7acdee30694f83c7777cedea33648
Eyal <eyalcohen308@gmail.com>
2018-12-18 10:59:43 +0200

    fixed the num class

--- Num.cpp
+++ Num.cpp
@@ -8,11 +8,9 @@
  * get string and convert it to double.
  * @param num string represent double.
  */
-Num::Num(string number) {
-    this->num = stod(number);
+Num::Num(string string_num) {
+    this->num = stod(string_num);
 }
-
-Num::Num(double num) : num(num) {}
 
 /**
  * get the number value.
@@ -33,4 +31,15 @@ double Num::getNum() const {
 ostream &operator<<(ostream &stream, const Num &num) {
     stream << (to_string(num.getNum()));
     return stream;
+}
+
+Num::Num(double number) {
+    this->num = number;
+}
+/**
+ * return the num value.
+ * @return num value by double.
+ */
+double Num::calculate() {
+    return num;
 }
