65ada0daed9b9815d1ae83570e544e278102bdde
shlaskt <shlaski@gmail.com>
2018-12-21 13:43:07 +0200

    update conditional

---
+++ Commands/WhileCommand.h
@@ -0,0 +1,18 @@
+//
+// Created by tomer on 12/21/18.
+//
+
+#ifndef FLIGHTSIMULATOR_WHILECOMMAND_H
+#define FLIGHTSIMULATOR_WHILECOMMAND_H
+
+
+#include "ConditionCommand.h"
+
+class WhileCommand : ConditionCommand{
+    VarDataBase varDataBase;
+public:
+    virtual void doCommand(vector<string>::iterator &itor, DataReaderServer* server);
+};
+
+
+#endif //FLIGHTSIMULATOR_WHILECOMMAND_H
